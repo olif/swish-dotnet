d5df3ad273bdca9dc6a2872cc4a03ac7206ad3c4
Niklas Olofsson <niklas.olofsson@ngenic.se>
2016-06-08 20:58:27 +0200

    Converted from .net core rc1 to rc2

--- tests/Client.IntegrationTests/IntegrationTests.cs
+++ tests/Client.IntegrationTests/IntegrationTests.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using System.IO;
 using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Threading.Tasks;
-using NSubstitute;
 using Xunit;
 
 namespace Client.IntegrationTests
@@ -13,7 +12,6 @@ namespace Client.IntegrationTests
 
         public IntegrationTests()
         {
-
             configuration = new TestConfig("1231181189");
         }
 
