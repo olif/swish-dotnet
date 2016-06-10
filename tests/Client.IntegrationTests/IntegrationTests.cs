2f2721ada0dfb7c488f1abcab15b54e89c3bf1fb
Niklas Olofsson <niklas.olofsson@olif.nu>
2016-06-10 19:58:35 +0200

    Removed unused using

--- tests/Client.IntegrationTests/IntegrationTests.cs
+++ tests/Client.IntegrationTests/IntegrationTests.cs
@@ -1,5 +1,4 @@
-﻿using System.IO;
-using System.Security.Cryptography.X509Certificates;
+﻿using System.Security.Cryptography.X509Certificates;
 using System.Threading;
 using System.Threading.Tasks;
 using Xunit;
